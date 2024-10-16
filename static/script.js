f77010d87ca7f2b5008908d037f662078819715b
Rafael <raphaelmenezesvill@gmail.com>
2024-10-16 20:47:18 -0300

    A pagina está funcional. Agora preciso adicionar estatisticas (palavras por minuto, tempo, contador de palavras) talvez esstilizar um pouco mais o site e colocar a opção de doação provavelmente um pix ou algo  do tipo

--- static/script.js
+++ static/script.js
@@ -1,33 +1,53 @@
 const startButton = document.getElementById("startButton")
+const eraseButton = document.getElementById("borracha")
 const outputDiv   = document.getElementById("output")
 const elemento_img_caderno = document.getElementById("caderno")
+const buttonImage = document.getElementById("imagem_do_botao_de_play")
+const buttonEraseImage =  document.getElementById("imagem_do_botao_de_apagar")
 
+
+let isListenning = false
 
 
 elemento_img_caderno.src = "static/img/caderno.png"
-
+buttonImage.src = "static/img/play.png"
+buttonEraseImage.src = "static/img/borracha.png"
 
 const recognition = new (window.SpeechRecognitionAlternative || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
 recognition.lang = 'en-US';
 
 
 recognition.onstart = () => {
-startButton.textContent = 'Listening...'
+    buttonImage.src = "static/img/pouse.png"
 }
 
 recognition.onresult = (data) => {
 const transcript =  data.results[0][0].transcript;
-outputDiv.textContent = transcript;
+outputDiv.textContent =  outputDiv.textContent +" "+transcript;
 }
 
 recognition.onend = () =>{
-startButton.textContent = 'Start Voice Input';
-recognition.start();
+    if(isListenning){
+        recognition.start();
+    }else{
+        recognition.stop()
+    }
+
 }
 
 
 startButton.addEventListener('click', () => {
-recognition.start();
+    if(!isListenning){
+        recognition.start();
+        isListenning = true;
+    }else{
+        isListenning = false;
+        buttonImage.src = "static/img/play.png"
+    }
+
 })
 
+eraseButton.addEventListener('click',() => {
+    outputDiv.textContent = ""
+})
 
